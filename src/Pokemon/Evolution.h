d0b5db67f23e5974119ac1d5a29c24cee940a5fa
nichwall <nicholaslwallace@gmail.com>
2015-08-11 18:03:06 +0000

    Began adding Pokemon.h

---
+++ src/Pokemon/Evolution.h
@@ -0,0 +1,12 @@
+// Handles evolution structures
+
+#ifndef POKEMON_EVOLUTION_H_
+#define POKEMON_EVOLUTION_H_
+
+struct evolutionDataStructure {
+	uint16_t method;
+	uint16_t parameter;
+	uint16_t target;
+};
+
+#endif
---
+++ src/Pokemon/Pokemon.h
@@ -0,0 +1,42 @@
+/* Manages Pokemon classes */
+
+#ifndef POKEMON_POKEMON_H_
+#define POKEMON_POKEMON_H_
+
+#include "../Language/Language.h"
+#include "../Type/Type.h"
+#include "../Move/Move.h"
+
+// Defines every Pokemon, but just what is required to define one
+class BasePokemon {
+	private:
+		// Pokemon name
+		std::string pokemonName;
+		// Types of the Pokemon
+		char type1;
+		char type2;
+		
+		// Evolution struct
+		struct evolutionyThings {
+			
+		};
+	public:
+		
+};
+
+// Defines Pokemon that have been caught, extends base
+class BoxPokemon: public BasePokemon {
+	
+};
+
+// Defines Pokemon in the party, extends box
+class PartyPokemon: public BoxPokemon {
+	
+};
+
+// Defines Pokemon in battle, extends party
+class BattlePokemon: public PartyPokemon {
+	
+};
+
+#endif
